1f6f3da6ad782b1de2013bff3d91d70d2361e7a5
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-30 16:23:56 -0300

    Create ft_set_newuser.sh

---
+++ setup_scripts/ft_set_newuser.sh
@@ -0,0 +1,21 @@
+##############################################################################
+#                                                                            #
+#                                                        :::      ::::::::   #
+#   ft_setnew_user.sh                                 :+:      :+:    :+:   #
+#                                                    +:+ +:+         +:+     #
+#   By: acesar-l <acesar-l@student.42sp.org>       +#+  +:+       +#+        #
+#                                                +#+#+#+#+#+   +#+           #
+#   Created: 2022/01/30 16:56:19 by acesar-l          #+#    #+#             #
+#   Updated: 2022/01/30 16:56:22 by acesar-l         ###   ########.fr       #
+#                                                                            #
+##############################################################################
+
+#!/bin/bash
+$NEW_USER=$1
+cd /
+sudo useradd -m -s /bin/bash $NEW_USER
+sudo addgroup user42
+sudo usermod -aG sudo $NEW_USER
+sudo usermod -aG user42 $NEW_USER
+echo $NEW_USER was created and added to sudo and user42 groups.
+echo A strong password policy was 'set' to the user.
