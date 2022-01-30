3678b7ce0a9cf7ef179da7ae6842974949f61af5
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-30 17:21:01 -0300

    Create ft_install_packages.sh

---
+++ setup_scripts/ft_install_packages.sh
@@ -0,0 +1,17 @@
+##############################################################################
+#                                                                            #
+#                                                       :::      ::::::::    #
+#   ft_install_packages.sh                             :+:      :+:    :+:   #
+#                                                    +:+ +:+         +:+     #
+#   By: acesar-l <acesar-l@student.42sp.org.br     +#+  +:+       +#+        #
+#                                                +#+#+#+#+#+   +#+           #
+#   Created: 2022/01/30 17:18:19 by acesar-l          #+#    #+#             #
+#   Updated: 2022/01/30 17:19:22 by acesar-l         ###   ########.fr       #
+#                                                                            #
+##############################################################################
+
+#!/bin/bash
+apt-get install sudo
+apt-get install vim
+apt-get install libpam-cracklib
+apt-get install lvm2
