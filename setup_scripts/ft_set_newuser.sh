0c07a24a59fa877cbab07a521d13650aefefea30
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-30 17:25:55 -0300

    Update ft_set_newuser.sh

--- setup_scripts/ft_set_newuser.sh
+++ setup_scripts/ft_set_newuser.sh
@@ -17,5 +17,6 @@ sudo useradd -m -s /bin/bash $NEW_USER
 sudo addgroup user42
 sudo usermod -aG sudo $NEW_USER
 sudo usermod -aG user42 $NEW_USER
+sudo passwd -n 2 -x 30 -w 7 $NEW_USER
 echo $NEW_USER was created and added to sudo and user42 groups.
 echo A strong password policy was 'set' to the user.
