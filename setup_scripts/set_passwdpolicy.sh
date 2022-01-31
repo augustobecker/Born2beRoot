b7789df07e2f33e88f3ebf4c99e66c60d9705f32
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-31 20:05:43 -0300

    Update set_passwdpolicy.sh

--- setup_scripts/set_passwdpolicy.sh
+++ setup_scripts/set_passwdpolicy.sh
@@ -13,30 +13,34 @@
 #!/bin/bash
 
 FILE=/etc/pam.d/common-password
+TXT=password_policy.txt
 
 sed -n '25 s/minlen=8/minlen=10/' $FILE
 sed -n '25 s/difok=3/difok=7/' $FILE
 sed -i '25 s/$/ maxrepeat=3 usercheck=1/' $FILE
 sed -i '25 s/$/ ucredit=-1 dcredit=-1 enforce_for_root/' $FILE
+touch TXT
 
-echo "******************************************************************************"
-echo "*                             PASSWORD POLICY                                *"
-echo "******************************************************************************"
-echo "* Your password must be at least 10 characters long.    | minlen=10          *"
-echo "*                                                       |                    *"
-echo "* It must contain an uppercase letter                   | ucredit=-1         *"
-echo "* and a number.                                         | dcredit=-1         *"
-echo "*                                                       |                    *"
-echo "* It must not contain more than 3 consecutive           | maxrepeat=3        *"
-echo "* identical characters.                                 |                    *"
-echo "*                                                       |                    *"
-echo "* The password must not include the name of the user.   | usercheck=1        *"
-echo "*                                                       |                    *"
-echo "* The password must have at least 7 characters that are | difok=7            *"
-echo "* not part of the former password.                      |                    *"
-echo "*                                                       |                    *"
-echo "* The root password has to comply with this policy.     | enforce_for_root   *"
-echo "******************************************************************************"
-echo "*                  THIS SCRIPT WILL SELF-DESTRUCT                            *"
-echo "* You can change those configurations in /etc/pam.d/common-password line 25  *"
-echo "******************************************************************************"
+echo "******************************************************************************" > TXT
+echo "*                             PASSWORD POLICY                                *" >> TXT
+echo "******************************************************************************" >> TXT
+echo "* Your password must be at least 10 characters long.    | minlen=10          *" >> TXT
+echo "*                                                       |                    *" >> TXT
+echo "* It must contain an uppercase letter                   | ucredit=-1         *" >> TXT
+echo "* and a number.                                         | dcredit=-1         *" >> TXT
+echo "*                                                       |                    *" >> TXT
+echo "* It must not contain more than 3 consecutive           | maxrepeat=3        *" >> TXT
+echo "* identical characters.                                 |                    *" >> TXT
+echo "*                                                       |                    *" >> TXT
+echo "* The password must not include the name of the user.   | usercheck=1        *" >> TXT
+echo "*                                                       |                    *" >> TXT
+echo "* The password must have at least 7 characters that are | difok=7            *" >> TXT
+echo "* not part of the former password.                      |                    *" >> TXT
+echo "*                                                       |                    *" >> TXT
+echo "* The root password has to comply with this policy.     | enforce_for_root   *" >> TXT
+echo "******************************************************************************" >> TXT
+echo "*                  THIS SCRIPT WILL SELF-DESTRUCT                            *" >> TXT
+echo "* You can change those configurations in /etc/pam.d/common-password line 25  *" >> TXT
+echo "******************************************************************************" >> TXT
+
+cat TXT
