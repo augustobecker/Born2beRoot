3624680a59d6b9e98bb3a6c89f1f0a90ca74a130
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-31 19:49:14 -0300

    Update set_passwdpolicy.sh

--- setup_scripts/set_passwdpolicy.sh
+++ setup_scripts/set_passwdpolicy.sh
@@ -19,15 +19,16 @@ sed -n '25 s/difok=3/difok=7/' $FILE
 sed -i '25 s/$/ maxrepeat=3 usercheck=1/' $FILE
 sed -i '25 s/$/ ucredit=-1 dcredit=-1 enforce_for_root/' $FILE
 
-echo "\
-****************************************************************************** \
-* Your password must be at least 10 characters long     | minlen=10          * \
-* It must contain an uppercase letter \                 | ucredit=-1         * \
-* and a number. \                                       | dcredit=-1         * \
-* It must not contain more than 3 consecutive           |                    * \
-* identical characters.                                 | maxrepeat=3        * \
-* The password must not include the name of the user    | usercheck=1        * \
-* The password must have at least 7 characters that are |                    * \
-* not part of the former password.                      | difok=7            * \ 
-* The root password has to comply with this polic       | enforce_for_root   * \ 
-****************************************************************************** "
+echo "******************************************************************************"
+echo "*                             PASSWORD POLICY                                *"
+echo "******************************************************************************"
+echo "* Your password must be at least 10 characters long.    | minlen=10          *"
+echo "* It must contain an uppercase letter \                 | ucredit=-1         *"
+echo "* and a number. \                                       | dcredit=-1         *"
+echo "* It must not contain more than 3 consecutive           | maxrepeat=3        *"
+echo "* identical characters.                                 |                    *"
+echo "* The password must not include the name of the user.   | usercheck=1        *"
+echo "* The password must have at least 7 characters that are | difok=7            *"
+echo "* not part of the former password.                      |                    *"
+echo "* The root password has to comply with this policy.     | enforce_for_root   *"
+echo "******************************************************************************"
