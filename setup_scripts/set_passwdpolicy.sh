6093d92877b2ea09283d670c2fac40db70594a21
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-31 19:45:01 -0300

    Update set_passwdpolicy.sh

--- setup_scripts/set_passwdpolicy.sh
+++ setup_scripts/set_passwdpolicy.sh
@@ -11,7 +11,23 @@
 ##############################################################################
 
 #!/bin/bash
-sed -n '25 s/minlen=8/minlen=10/' /etc/pam.d/common-password
-sed -n '25 s/difok=3/difok=7 \\/' /etc/pam.d/common-password
-sed -n -i '25 s/$/ ucredit=-1 dcredit=-1 maxrepeat=3 usercheck=1
-enforce_for_root' /etc/pam.d/common-password
+
+FILE=/etc/pam.d/common-password
+
+sed -n '25 s/minlen=8/minlen=10/' $FILE
+sed -n '25 s/difok=3/difok=7/' $FILE
+sed -i '25 s/$/ maxrepeat=3 usercheck=1/' $FILE
+sed -i '25 s/$/ ucredit=-1 dcredit=-1 enforce_for_root/' $FILE
+
+echo "\
+****************************************************************************** \
+* Your password must be at least 10 characters long     | minlen=10          * \
+* It must contain an uppercase letter \                 | ucredit=-1         * \
+* and a number. \                                       | dcredit=-1         * \
+* It must not contain more than 3 consecutive           |                    * \
+* identical characters.                                 | maxrepeat=3        * \
+* The password must not include the name of the user    | usercheck=1        * \
+* The password must have at least 7 characters that are |                    * \
+* not part of the former password.                      | difok=7            * \ 
+* The root password has to comply with this polic       | enforce_for_root   * \ 
+****************************************************************************** "
