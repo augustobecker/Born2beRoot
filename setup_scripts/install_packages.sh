d50bc62aa74e7cf864ea5385b161f6964db406f2
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-01 11:42:00 -0300

    Update install_packages.sh

--- setup_scripts/install_packages.sh
+++ setup_scripts/install_packages.sh
@@ -1,16 +1,17 @@
-##############################################################################
-#                                                                            #
-#                                                       :::      ::::::::    #
-#   install_packages.sh                             :+:      :+:    :+:   #
-#                                                    +:+ +:+         +:+     #
-#   By: acesar-l <acesar-l@student.42sp.org.br     +#+  +:+       +#+        #
-#                                                +#+#+#+#+#+   +#+           #
-#   Created: 2022/01/30 17:18:19 by acesar-l          #+#    #+#             #
-#   Updated: 2022/01/30 17:19:22 by acesar-l         ###   ########.fr       #
-#                                                                            #
-##############################################################################
-
 #!/bin/bash
+
+#
+#                                                                            
+#                                                       :::      ::::::::    
+#   install_packages.sh                             :+:      :+:    :+:   
+#                                                    +:+ +:+         +:+     
+#   By: acesar-l <acesar-l@student.42sp.org.br     +#+  +:+       +#+        
+#                                                +#+#+#+#+#+   +#+           
+#   Created: 2022/01/30 17:18:19 by acesar-l          #+#    #+#             
+#   Updated: 2022/01/30 17:19:22 by acesar-l         ###   ########.fr       
+#                                                                            
+#
+
 apt-get install sudo
 apt-get install vim
 apt-get install libpam-cracklib
