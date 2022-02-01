82572add5b872935b7ee01314014eed8f09a8e8f
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-01 11:40:44 -0300

    Update set_newuser.sh

--- setup_scripts/set_newuser.sh
+++ setup_scripts/set_newuser.sh
@@ -1,21 +1,24 @@
-##############################################################################
-#                                                                            #
-#                                                        :::      ::::::::   #
-#   set_newuser.sh                                    :+:      :+:    :+:    #
-#                                                    +:+ +:+         +:+     #
-#   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        #
-#                                                +#+#+#+#+#+   +#+           #
-#   Created: 2022/01/30 16:56:19 by acesar-l          #+#    #+#             #
-#   Updated: 2022/01/30 16:56:22 by acesar-l         ###   ########.fr       #
-#                                                                            #
-##############################################################################
-
 #!/bin/bash
+
+#
+#                                                                            
+#                                                        :::      ::::::::   
+#   set_newuser.sh                                    :+:      :+:    :+:    
+#                                                    +:+ +:+         +:+     
+#   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        
+#                                                +#+#+#+#+#+   +#+           
+#   Created: 2022/01/30 16:56:19 by acesar-l          #+#    #+#             
+#   Updated: 2022/01/30 16:56:22 by acesar-l         ###   ########.fr       
+#                                                                            
+#
+
 NEW_USER=$1
+
 cd /
 sudo useradd -m -s /bin/bash $NEW_USER
 sudo addgroup friends
 sudo usermod -aG friends $NEW_USER
 sudo passwd -n 2 -x 30 -w 7 $NEW_USER
+
 echo $NEW_USER was created and added to friends group.
-echo A strong password policy was 'set' to the user.
+echo "A strong password policy was set to the user."
