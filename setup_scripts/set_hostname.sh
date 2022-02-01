22d5d64d9b6ca143d60c1dd5e22cf504067ebb3b
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-01 11:45:27 -0300

    Update set_hostname.sh

--- setup_scripts/set_hostname.sh
+++ setup_scripts/set_hostname.sh
@@ -1,19 +1,21 @@
-##############################################################################
-#                                                                            #
-#                                                       :::      ::::::::    #
-#   set_hostname.sh                                 :+:      :+:    :+:      #
-#                                                    +:+ +:+         +:+     #
-#   By: acesar-l <acesar-l@student.42sp.org.br     +#+  +:+       +#+        #
-#                                                +#+#+#+#+#+   +#+           #
-#   Created: 2022/01/30 16:24:19 by acesar-l          #+#    #+#             #
-#   Updated: 2022/01/30 16:24:22 by acesar-l         ###   ########.fr       #
-#                                                                            #
-##############################################################################
-
 #!/bin/bash
+
+#
+#                                                                            
+#                                                       :::      ::::::::    
+#   set_hostname.sh                                 :+:      :+:    :+:      
+#                                                    +:+ +:+         +:+     
+#   By: acesar-l <acesar-l@student.42sp.org.br     +#+  +:+       +#+        
+#                                                +#+#+#+#+#+   +#+           
+#   Created: 2022/01/30 16:24:19 by acesar-l          #+#    #+#             
+#   Updated: 2022/01/30 16:24:22 by acesar-l         ###   ########.fr       
+#                                                                            
+#
+
 NEW_HOSTNAME=$1
+
 cd /
-sed -n "s/$HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
+sed -i "s/$HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
 echo $NEW_HOSTNAME > /etc/hostname
 echo $NEW_HOSTNAME > /proc/sys/kernel/hostname
 echo Hostname was changed to $NEW_HOSTNAME
