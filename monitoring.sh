ac24012829cfeaacf080b5876b00dd35b2d4cef8
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-01 20:28:32 -0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -15,14 +15,15 @@
 DATE=`(date +'%a %b %d %T %Y')`
 
 LAST_BOOT=`(who -b | head -n 1 | awk '{ print $3, $4}')`
-PROCESSORS=`(cat /proc/cpuinfo | grep processor | wc -l)`
+PHYSICAL_CPU=`(grep "physical id" /proc/cpuinfo | sort -u | wc -l)`
+VIRTUAL_CPU=`(grep "processor" /proc/cpuinfo | sort -u | wc -l)`
 IP_ADDRESS=`(sudo ifconfig | grep 'broadcast' | awk '{ print $2 }')`
 MAC_ADDRESS=`(sudo ifconfig | grep 'ether' | awk '{ print $2 }')`
 
 echo "Broadcast message from root@$HOSTNAME (tty1)" \($DATE\)
 echo "  #Architecture:   "
-echo "  #CPU Physical: "
-echo "  #vCPU: "$PROCESSORS
+echo "  #CPU Physical: "$PHYSICAL_CPU
+echo "  #vCPU: "$VIRTUAL_CPU
 echo "  #Memory Usage:   "
 echo "  #Disk Usage:     "
 echo "  #CPU load:       "
