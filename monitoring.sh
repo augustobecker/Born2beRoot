b23b79eb142c20a3a525da2358cb4972f52c8e7c
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-01 18:06:16 -0300

    Create monitoring.sh

---
+++ monitoring.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+#                                                                            
+#                                                        :::      ::::::::   
+#   monitoring.sh                                      :+:      :+:    :+:   
+#                                                    +:+ +:+         +:+     
+#   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        
+#                                                +#+#+#+#+#+   +#+          
+#   Created: 2021/02/01 16:58:10 by acesar-l          #+#    #+#             
+#   Updated: 2021/02/01 17:00:41 by acesar-l         ###   ########.fr       
+#                                                                            
+# 
+
+DATE=`(date +'%a %b %d %T %Y')`
+
+LAST_BOOT=`(who -b | head -n 1 | awk '{ print $3, $4}')`
+PROCESSORS=`(cat /proc/cpuinfo | grep processor | wc -l)`
+IP_ADRESS=`(sudo ifconfig | grep 'broadcast' | awk '{ print $2 }')`
+MAC_ADRESS=`(sudo ifconfig | grep 'ether' | awk '{ print $2 }')`
+
+echo "Broadcast message from root@$HOSTNAME (tty1)" \($DATE\)
+echo "  #Architecture:   "
+echo "  #CPU Physical: "
+echo "  #vCPU: "$PROCESSORS
+echo "  #Memory Usage:   "
+echo "  #Disk Usage:     "
+echo "  #CPU load:       "
+echo "  #Last boot: "$LAST_BOOT
+echo "  #LVM use:        "
+echo "  #Connexions TCP: "
+echo "  #User log:       "
+echo "  Network: IP "$IP_ADDRESS \($MAC_ADDRESS\)
+echo "  Sudo:            "
