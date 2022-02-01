f9435a808af9207edd5b345b2ced9a715b8f341d
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-01 18:29:13 -0300

    Create requirements.sh

---
+++ evaluation_scripts/requirements.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+#                                                                            
+#                                                        :::      ::::::::   
+#   requirements.sh                                    :+:      :+:    :+:  
+#                                                    +:+ +:+         +:+     
+#   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        
+#                                                +#+#+#+#+#+   +#+          
+#   Created: 2021/02/01 18:58:10 by acesar-l          #+#    #+#             
+#   Updated: 2021/02/01 19:00:41 by acesar-l         ###   ########.fr       
+#                                                                            
+# 
+
+USER=$1
+
+head -n 2 /etc/os-release
+cd /home/$USER
+/usr/sbin/aa-status
+ss -tunip
+/usr/sbin/ufw status
