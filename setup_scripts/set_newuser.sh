876e9817f33a457d68a66e0fe44418b15d668aa8
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-02 18:16:17 -0300

    Update set_newuser.sh

--- setup_scripts/set_newuser.sh
+++ setup_scripts/set_newuser.sh
@@ -13,12 +13,13 @@
 #
 
 NEW_USER=$1
+GROUP=$2
 
 cd /
 sudo useradd -m -s /bin/bash $NEW_USER
-sudo addgroup friends
-sudo usermod -aG friends $NEW_USER
+sudo addgroup $GROUP
+sudo usermod -aG $GROUP $NEW_USER
 sudo passwd -n 2 -x 30 -w 7 $NEW_USER
 
-echo $NEW_USER was created and added to friends group.
+echo $NEW_USER was created and added to $GROUP group.
 echo "A strong password policy was set to the user."
