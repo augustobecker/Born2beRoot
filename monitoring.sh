de6b4a81f4a34d736624b50ba7fe774a0ee5e2f7
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-02 00:04:17 -0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -14,22 +14,29 @@
 
 DATE=`(date +'%a %b %d %T %Y')`
 
-LAST_BOOT=`(who -b | head -n 1 | awk '{ print $3, $4}')`
+ARCHITECTURE=`(uname -a)`
+
 PHYSICAL_CPU=`(grep "physical id" /proc/cpuinfo | sort -u | wc -l)`
+
 VIRTUAL_CPU=`(grep "processor" /proc/cpuinfo | sort -u | wc -l)`
+
+LAST_BOOT=`(who -b | head -n 1 | awk '{ print $3, $4}')`
+
 IP_ADDRESS=`(sudo ifconfig | grep 'broadcast' | awk '{ print $2 }')`
 MAC_ADDRESS=`(sudo ifconfig | grep 'ether' | awk '{ print $2 }')`
 
+COMMANDS_EXECUTED=`(grep "COMMAND=" /var/log/sudo/sudo.log | wc -l)`
+
 echo "Broadcast message from root@$HOSTNAME (tty1)" \($DATE\)
-echo "  #Architecture:   "
+echo "  #Architecture: "$ARCHITECTURE
 echo "  #CPU Physical: "$PHYSICAL_CPU
 echo "  #vCPU: "$VIRTUAL_CPU
-echo "  #Memory Usage:   "
-echo "  #Disk Usage:     "
-echo "  #CPU load:       "
+echo "  #Memory Usage: "
+echo "  #Disk Usage: "
+echo "  #CPU load: "
 echo "  #Last boot: "$LAST_BOOT
-echo "  #LVM use:        "
+echo "  #LVM use: "
 echo "  #Connexions TCP: "
-echo "  #User log:       "
+echo "  #User log: "
 echo "  #Network: IP "$IP_ADDRESS \($MAC_ADDRESS\)
-echo "  #Sudo:            "
+echo "  #Sudo: "$COMMANDS_EXECUTED cmd
