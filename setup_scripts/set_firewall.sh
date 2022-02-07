acc2e2791f3b1101034fbd868b69455f5304b8ce
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-07 12:27:46 -0300

    Create set_firewall.sh

---
+++ setup_scripts/set_firewall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+#                                                                            
+#                                                       :::      ::::::::    
+#   set_firewall.sh                                    :+:      :+:    :+:   
+#                                                    +:+ +:+         +:+     
+#   By: acesar-l <acesar-l@student.42sp.org.br     +#+  +:+       +#+        
+#                                                +#+#+#+#+#+   +#+           
+#   Created: 2022/02/07 12:23:19 by acesar-l          #+#    #+#             
+#   Updated: 2022/02/07 12:25:22 by acesar-l         ###   ########.fr       
+#                                                                            
+#
+
+PORT="4242"
+
+sudo ufw enable
+sudo ufw allow {PORT}
+sudo ufw status
