09cc9ca68e463a63351b909b601f69a1ee49244e
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-07 20:49:43 -0300

    Update set_sshserver.sh

--- setup_scripts/set_sshserver.sh
+++ setup_scripts/set_sshserver.sh
@@ -12,8 +12,14 @@
 #                                                                            
 # 
 
-SSH='/etc/ssh/sshd_config'
-IP=`(ip addr show | awk '{print $2}' | sed -n '9p')`
+      SSH='/etc/ssh/sshd_config'
+  NETWORK='/etc/network/interfaces'
+
+      IP=$(ip addr show | awk '{print $2}' | sed -n '9p')
+NET_MASK=$()
+ GATEWAY=$()
+ 
+BROADCAST=$(ip addr show | awk '{print $4}' | sed -n '9p')
 
 sed -i 's/#Port 22/Port 4242/' $SSH
 sed -i 's/#PermitRootLogin/PermitRootLogin/' $SSH
@@ -23,3 +29,8 @@ echo "Verify if the ssh_server is active:"
 sudo service ssh status
 echo "To connect via SSH from other terminal:"
 echo ssh $USER@$IP -p 4242
+
+sed -i '12 s/dhcp/static/g' $NETWORK
+echo address $IP >> $NETWORK
+echo netmask $NET_MASK >> $NETWORK
+echo gateway $GATEWAY >> $NETWORK
