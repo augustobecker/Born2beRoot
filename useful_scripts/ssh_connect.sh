3471294a2e4b3aef1ae793eef13aa9005cc6e4df
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-07 16:18:33 -0300

    Create ssh_connect.sh

---
+++ useful_scripts/ssh_connect.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+#                                                                            
+#                                                        :::      ::::::::   
+#   ssh_connect.sh                                      :+:      :+:    :+:   
+#                                                    +:+ +:+         +:+     
+#   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        
+#                                                +#+#+#+#+#+   +#+          
+#   Created: 2021/02/04 12:58:10 by acesar-l          #+#    #+#             
+#   Updated: 2021/02/04 13:00:41 by acesar-l         ###   ########.fr       
+#                                                                            
+# 
+
+PORT="4242"
+
+echo ssh $USER@$IP -p $PORT
