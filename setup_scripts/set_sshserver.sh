19764c44d6c67e761f1460c07b4263c03485e680
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-07 21:19:44 -0300

    Update set_sshserver.sh

--- setup_scripts/set_sshserver.sh
+++ setup_scripts/set_sshserver.sh
@@ -15,22 +15,24 @@
       SSH='/etc/ssh/sshd_config'
   NETWORK='/etc/network/interfaces'
 
-      IP=$(ip addr show | awk '{print $2}' | sed -n '9p')
+      IP=$(hostname -I)
 NET_MASK=$()
  GATEWAY=$()
- 
 BROADCAST=$(ip addr show | awk '{print $4}' | sed -n '9p')
 
-sed -i 's/#Port 22/Port 4242/' $SSH
-sed -i 's/#PermitRootLogin/PermitRootLogin/' $SSH
-sed -i 's/prohibit-password/no/' $SSH
+sed -i 's/#Port 22/Port 4242/'                  $SSH
+sed -i 's/#PermitRootLogin/PermitRootLogin/'    $SSH
+sed -i 's/prohibit-password/no/'                $SSH
 
 echo "Verify if the ssh_server is active:"
 sudo service ssh status
 echo "To connect via SSH from other terminal:"
 echo ssh $USER@$IP -p 4242
 
-sed -i '12 s/dhcp/static/g' $NETWORK
-echo address $IP >> $NETWORK
-echo netmask $NET_MASK >> $NETWORK
-echo gateway $GATEWAY >> $NETWORK
+sed -i '11 s/auto/allow-hotplug/'      $NETWORK
+sed -i '12 s/dhcp/static/'             $NETWORK
+echo "      address "$IP            >> $NETWORK
+echo "      broadcast "$NET_MASK    >> $NETWORK
+echo "      netmask "$NET_MASK      >> $NETWORK
+echo "      gateway "$GATEWAY       >> $NETWORK
+echo iface enp0s3 inet6 auto        >> $NETWORK
