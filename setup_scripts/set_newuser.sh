16bd69e9f95cae076b46691bf572bae6cbceb7db
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-08 17:01:47 -0300

    Update set_newuser.sh

--- setup_scripts/set_newuser.sh
+++ setup_scripts/set_newuser.sh
@@ -8,18 +8,17 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        
 #                                                +#+#+#+#+#+   +#+           
 #   Created: 2022/01/30 16:56:19 by acesar-l          #+#    #+#             
-#   Updated: 2022/01/30 16:56:22 by acesar-l         ###   ########.fr       
+#   Updated: 2022/02/08 17:06:22 by acesar-l         ###   ########.fr       
 #                                                                            
 #
 
-NEW_USER=$1
-GROUP=$2
+   NEW_USER=$1
+      GROUP=$2
+GROUP_CHECK=$(getent group | grep "$GROUP:" | wc -l)
 
-cd /
 sudo useradd -m -s /bin/bash $NEW_USER
-sudo addgroup $GROUP
+if [ $GROUP_CHECK -ge 1 ]; then addgroup $GROUP; fi
 sudo usermod -aG $GROUP $NEW_USER
 sudo passwd -n 2 -x 30 -w 7 $NEW_USER
 
 echo $NEW_USER was created and added to $GROUP group.
-echo "A strong password policy was set to the user."
