8431164e0c232f0c84dd01c9d2e7b35c6c183476
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-19 16:51:44 -0300

    Colorful hostname.sh

--- useful_scripts/hostname.sh
+++ useful_scripts/hostname.sh
@@ -8,14 +8,17 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br     +#+  +:+       +#+        
 #                                                +#+#+#+#+#+   +#+           
 #   Created: 2022/01/30 16:24:19 by acesar-l          #+#    #+#             
-#   Updated: 2022/02/08 16:24:22 by acesar-l         ###   ########.fr       
+#   Updated: 2022/02/19 16:54:22 by acesar-l         ###   ########.fr       
 #                                                                            
 #
 
 NEW_HOSTNAME=$1
 
+GREEN="\033[0;32m"
+RESET="\033[0m"
+
 cd /
 sed -i "s/$HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
 echo $NEW_HOSTNAME > /etc/hostname
 echo $NEW_HOSTNAME > /proc/sys/kernel/hostname
-echo Hostname was changed to $NEW_HOSTNAME
+echo -e $GREEN"Hostname was changed to "$NEW_HOSTNAME $RESET
