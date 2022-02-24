d6097301bb1d791c6c38ef722998b4f5945a6338
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-24 11:40:27 -0300

    Update evaluation_check.sh

--- evaluation_scripts/evaluation_check.sh
+++ evaluation_scripts/evaluation_check.sh
@@ -8,41 +8,36 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br     +#+  +:+       +#+        
 #                                                +#+#+#+#+#+   +#+           
 #   Created: 2022/02/08 16:24:19 by acesar-l          #+#    #+#             
-#   Updated: 2022/02/24 11:24:22 by acesar-l         ###   ########.fr       
+#   Updated: 2022/02/24 11:42:22 by acesar-l         ###   ########.fr       
 #                                                                            
 #
 
            GREEN="\033[0;32m"
              RED="\033[0;31m"
            RESET="\033[0m"
-           
-             PAM='/etc/pam.d/common-password'
-            FILE='/etc/login.defs'
-            
+
              LVM=$(lsblk | grep lvm | wc -l)
       LOGIN_USER=$(cat /etc/passwd | grep acesar-l)
     LOGIN_GROUPS=$(groups acesar-l | grep sudo | grep user42)
-     USER_POLICY=$(sed -n '160 p; 161 p; 162 p' $FILE | paste -s -d ',' | sed 's/,/, /g')
-       CHECK_PAM=$(sed -n '25 p' $PAM | awk '{ print $4, $5, $6, $7, $8, $9, $10, $11}')
 USER_GROUP_CHECK=$(groups acesar-l | grep user42 | grep sudo | wc -l)
  
 if [ ${LVM} -ge 2 ]
 then 
-       echo -e Partitions: ${GREEN}"OK"${RESET}
+       echo -e "Partitions:       ${GREEN}[OK]${RESET}"
 else
-       echo -e Partitions: ${RED}$"KO"${RESET}
+       echo -e "Partitions:       ${RED}[KO]${RESET}"
 fi
 
 if  [[ "${HOSTNAME}" == "acesar-l42" ]]
 then
-       echo -e Hostname login42: ${GREEN}"OK"${RESET}
+       echo -e "Hostname login42: ${GREEN}[OK]${RESET}"
 else
-       echo -e Hostname login42: ${RED}$"KO"${RESET}
+       echo -e "Hostname login42: ${RED}[KO]${RESET}"
 fi
 
 if [ $USER_GROUP_CHECK == 0 ]
 then
-	echo -e User login42: ${RED}$"KO"${RESET}
+	echo -e "User login42:     ${RED}[KO]${RESET}"
 else
-	echo -e User login42: ${GREEN}"OK"${RESET}
+	echo -e "User login42:     ${GREEN}[OK]${RESET}"
 fi
