65b1d040c825c86a24d1fc15d7072e9f3c688585
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-24 11:50:07 -0300

    Update hostname.sh

--- useful_scripts/hostname.sh
+++ useful_scripts/hostname.sh
@@ -8,7 +8,7 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br     +#+  +:+       +#+        
 #                                                +#+#+#+#+#+   +#+           
 #   Created: 2022/01/30 16:24:19 by acesar-l          #+#    #+#             
-#   Updated: 2022/02/19 16:54:22 by acesar-l         ###   ########.fr       
+#   Updated: 2022/02/24 11:54:22 by acesar-l         ###   ########.fr       
 #                                                                            
 #
 
@@ -21,4 +21,4 @@ cd /
 sed -i "s/$HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
 echo $NEW_HOSTNAME > /etc/hostname
 echo $NEW_HOSTNAME > /proc/sys/kernel/hostname
-echo -e $GREEN"Hostname was changed to "$NEW_HOSTNAME $RESET
+echo -e "Hostname was changed to"$GREEN $NEW_HOSTNAME $RESET
