3c5fc8abc90068527962009fe53d36b8d02b4c37
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-24 11:32:41 -0300

    Update evaluation_check.sh

--- evaluation_scripts/evaluation_check.sh
+++ evaluation_scripts/evaluation_check.sh
@@ -8,45 +8,41 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br     +#+  +:+       +#+        
 #                                                +#+#+#+#+#+   +#+           
 #   Created: 2022/02/08 16:24:19 by acesar-l          #+#    #+#             
-#   Updated: 2022/02/08 16:24:22 by acesar-l         ###   ########.fr       
+#   Updated: 2022/02/24 11:24:22 by acesar-l         ###   ########.fr       
 #                                                                            
 #
 
-       GREEN="\033[0;32m"
-         RED="\033[0;31m"
-       RESET="\033[0m"
-       
-         PAM='/etc/pam.d/common-password'
-        FILE='/etc/login.defs'
-         LVM=$(lsblk | grep lvm | wc -l)
-  LOGIN_USER=$(cat /etc/passwd | grep acesar-l)
-LOGIN_GROUPS=$(groups acesar-l | grep sudo | grep user42)
- USER_POLICY=$(sed -n '160 p; 161 p; 162 p' $FILE | paste -s -d ',' | sed 's/,/, /g')
-   CHECK_PAM=$(sed -n '25 p' $PAM | awk '{ print $4, $5, $6, $7, $8, $9, $10, $11}')
-   
-          OK=$(echo -e ${GREEN}$"OK"${RESET})
-          KO=$(echo -e ${RED}$"KO"${RESET})
+           GREEN="\033[0;32m"
+             RED="\033[0;31m"
+           RESET="\033[0m"
+           
+             PAM='/etc/pam.d/common-password'
+            FILE='/etc/login.defs'
+            
+             LVM=$(lsblk | grep lvm | wc -l)
+      LOGIN_USER=$(cat /etc/passwd | grep acesar-l)
+    LOGIN_GROUPS=$(groups acesar-l | grep sudo | grep user42)
+     USER_POLICY=$(sed -n '160 p; 161 p; 162 p' $FILE | paste -s -d ',' | sed 's/,/, /g')
+       CHECK_PAM=$(sed -n '25 p' $PAM | awk '{ print $4, $5, $6, $7, $8, $9, $10, $11}')
+USER_GROUP_CHECK=$(groups acesar-l | grep user42 | grep sudo | wc -l)
  
-echo "1. Check if there's at least 2 encrypted partitions using LVM:"
-echo "1. Partitions = "$LVM
+if [ ${LVM} -ge 2 ]
+then 
+       echo -e Partitions: ${GREEN}"OK"${RESET}
+else
+       echo -e Partitions: ${RED}$"KO"${RESET}
+fi
 
-if [ ${LVM} -ge 2 ]; then ${OK}; else ${KO} ; fi
+if  [[ "${HOSTNAME}" == "acesar-l42" ]]
+then
+       echo -e Hostname login42: ${GREEN}"OK"${RESET}
+else
+       echo -e Hostname login42: ${RED}$"KO"${RESET}
+fi
 
-echo "2. Check if the hostname of the virtual machine is my login ending
-with 42 (acesar-l42):"
-echo "2. hostname: "$HOSTNAME
-
-if   [[ "${HOSTNAME}" > "acesar-l42" ]]; then ${KO};           \
-elif [[ "${HOSTNAME}" > "acesar-l42" ]]; then ${KO};           \
-else                                          ${OK}; fi
-
-echo "3. A user with my login (acesar-l) as username has to be present"
-echo "   and belong to the user42 and sudo groups:"
-echo "3. user = $LOGIN_USER"
-echo " groups = $LOGIN_GROUPS"
-echo "4. Check if a strong password policy was implemented:"
-
-chage -l acesar-l
-
-$CHECK_PAM
-$USER POLICY
+if [ $USER_GROUP_CHECK == 0 ]
+then
+	echo -e User login42: ${RED}$"KO"${RESET}
+else
+	echo -e User login42: ${GREEN}"OK"${RESET}
+fi
