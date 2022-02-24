7abb62c4149b7f7c405c7b7825bbbc41d1bcf420
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-24 01:09:36 -0300

    Update set_loginuser.sh

--- setup_scripts/set_loginuser.sh
+++ setup_scripts/set_loginuser.sh
@@ -8,7 +8,7 @@
 #   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        
 #                                                +#+#+#+#+#+   +#+           
 #   Created: 2022/01/31 16:56:19 by acesar-l          #+#    #+#             
-#   Updated: 2022/02/08 16:48:22 by acesar-l         ###   ########.fr       
+#   Updated: 2022/02/24 01:08:22 by acesar-l         ###   ########.fr       
 #                                                                            
 #
 
@@ -21,11 +21,11 @@ LOGIN_GROUPS=$(groups $LOGIN42 | grep sudo | grep user42)
  CREATE_USER=$(sudo useradd -m -s /bin/bash $LOGIN42)
 USER42_GROUP=$(getent group | grep user42: | wc -l)
  
-if [ $LOGIN_USER -ge 1 ]; then $CREATE_USER; fi
-if [ $USER42_GROUP -ge 1 ]; then addgroup user42; fi
+if [ $LOGIN_USER -ge 1 ];  then $CREATE_USER;    fi
+if [ $USER42_GROUP == 0 ]; then addgroup user42; fi
+
 sudo usermod -aG user42 $LOGIN42
 sudo usermod -aG sudo $LOGIN42
-
 sudo passwd -n 2 -x 30 -w 7 $LOGIN42
 
-echo -e $GREEN $LOGIN42 is present and is a part of user42 and sudo groups. $RESET
+echo -e $GREEN$LOGIN42 is present and is a part of user42 and sudo groups. $RESET
