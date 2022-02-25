223b25c979b957c6b350730575443d012a6ef425
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-25 14:50:24 -0300

    Update monitoring.sh

--- scripts/subject/monitoring.sh
+++ scripts/subject/monitoring.sh
@@ -31,15 +31,15 @@
 COMMANDS_EXEC=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
 
 wall "
-#Architecture	  : $ARCHITECTURE
-#CPU Physical	  : $PHYSICAL_CPU
-#vCPU        	  : $VIRTUAL_CPU
-#Memory Usage	  : $RAM_PERCNT
-#Disk Usage	    : $DISK_USED/${DISK_TOTAL}b ($DISK_PERCNT)
-#CPU load	      : $CPU_LOAD
+#Architecture	: $ARCHITECTURE
+#CPU Physical	: $PHYSICAL_CPU
+#vCPU        	: $VIRTUAL_CPU
+#Memory Usage	: $RAM_PERCNT
+#Disk Usage	   	: $DISK_USED/${DISK_TOTAL}b ($DISK_PERCNT)
+#CPU load	    : $CPU_LOAD
 #Last boot	    : $LAST_BOOT
-#LVM use	      : $LVM_USE
+#LVM use	    : $LVM_USE
 #Connexions TCP	: $TCP_CONX ESTABLISHED
-#User log	      : $USER_LOG
-#Network	      : IP $IP_ADDRESS (${MAC_ADDRESS})
-#Sudo		        : $COMMANDS_EXEC cmd"
+#User log	    : $USER_LOG
+#Network	    : IP $IP_ADDRESS (${MAC_ADDRESS})
+#Sudo		    : $COMMANDS_EXEC cmd"
